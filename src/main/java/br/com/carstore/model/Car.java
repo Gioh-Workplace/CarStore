74c52bb05718ec079c8fffa419e2b790b39a6c6c
Giovani <giovanijusto54@gmail.com>
2023-10-20 09:06:50 -0300

    Upload v2 - Lab 2

---
+++ src/main/java/br/com/carstore/model/Car.java
@@ -0,0 +1,12 @@
+package br.com.carstore.model;
+
+public class Car {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String nome){
+        this.name = nome;
+    }
+}
